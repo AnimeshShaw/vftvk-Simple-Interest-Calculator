1fc34eb44e0681b51905b4145d7fe82bfafce5d8
AnimeshShaw <anim3.sh@outlook.com>
2021-06-09 08:48:29 +0530

    all tasks and requirements met

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# coding-project-template
+# Simple Interest Calculator
+
+Completed as part of a peer-Review Project for the course *Introduction to Cloud Development with HTML, CSS, JavaScript* as part of the *IBM Cloud Development Foundations Specialization*
--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var p = parseFloat(document.getElementById("principal").value);
+    var r = parseFloat(document.getElementById("rate").value);
+    var t = parseInt(document.getElementById("years").value);
+
+    var int = (p * t * r) / 100;
+
+    var maturity_year = new Date().getFullYear() + t;
+
+    document.getElementById("results").style.display = 'block';
+    document.getElementById("ph1").textContent = p;
+    document.getElementById("ph2").textContent = r.toString() + "%";
+    document.getElementById("ph3").textContent = int;
+    document.getElementById("ph4").textContent = maturity_year;
+
 }
-        
+
+function showRate() {
+    rate = document.getElementById("rate").value;
+    document.getElementById("rate-value").innerText = rate + "%";
+}
+
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    if (!(parseInt(principal) > 0)) {
+        alert("Enter a number greater than 0.");
+        document.getElementById("compute").disabled = true
+        document.getElementById("principal").focus();
+    } else {
+        document.getElementById("compute").disabled = false
+    }
+}
